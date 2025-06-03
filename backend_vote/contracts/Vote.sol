84d12268b5e2bf08061c56363283d0e4c1c0670e
Arthur <arthurlucas92@orange.fr>
2025-06-03 14:41:52 +0200

    work done

--- backend_vote/contracts/Vote.sol
+++ backend_vote/contracts/Vote.sol
@@ -9,6 +9,9 @@ contract Vote {
 
     Candidate[] public candidates;
     mapping(address => bool) public hasVoted;
+    address[] public voters;
+    uint public totalVotes;
+    uint public constant MAX_VOTES = 2;
 
     constructor(string[] memory candidateNames) {
         for (uint i = 0; i < candidateNames.length; i++) {
@@ -16,9 +19,43 @@ contract Vote {
         }
     }
 
+    function resetVotes() public {
+        for (uint i = 0; i < candidates.length; i++) {
+            candidates[i].voteCount = 0;
+        }
+        for (uint i = 0; i < voters.length; i++) {
+            hasVoted[voters[i]] = false;
+        }
+        totalVotes = 0;
+        delete voters;
+    }
+
+    function hasAlreadyVoted() public view returns (bool) {
+        return hasVoted[msg.sender];
+    }
+
+    function isEqual() public view returns (bool) {
+        if (totalVotes < MAX_VOTES) {
+            return false;
+        }
+        // Cherche les deux scores les plus élevés
+        uint first = 0;
+        uint second = 0;
+        for (uint i = 0; i < candidates.length; i++) {
+            uint votes = candidates[i].voteCount;
+            if (votes > first) {
+                second = first;
+                first = votes;
+            } else if (votes > second) {
+                second = votes;
+            }
+        }
+        return (first == second && first != 0);
+    }
+
     function voteByName(string memory candidateName) public {
         require(!hasVoted[msg.sender], "Vous avez deja vote.");
-
+        require(totalVotes < MAX_VOTES, "Le nombre maximum de votes a ete atteint.");
         uint candidateIndex = candidates.length;
         for (uint i = 0; i < candidates.length; i++) {
             if (keccak256(bytes(candidates[i].name)) == keccak256(bytes(candidateName))) {
@@ -29,6 +66,8 @@ contract Vote {
         require(candidateIndex < candidates.length, "Le candidat n'existe pas.");
 
         hasVoted[msg.sender] = true;
+        voters.push(msg.sender);
+        totalVotes += 1;
         candidates[candidateIndex].voteCount += 1;
     }
 
